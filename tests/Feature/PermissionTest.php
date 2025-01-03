2825efbcfce59feb839a9601a8f095c8e6457177
Sara DM <ochuck.sdm@gmail.com>
2025-01-03 15:27:04 +0100

    fix: :memo: Removed Permission from migration. Added in Tests to ensure it works if need be.
    For now will stick to roles for authorization. Permission should still be available and working if it's needed later on. Test ensures it does and works as example code.

--- tests/Feature/PermissionTest.php
+++ tests/Feature/PermissionTest.php
@@ -1,8 +1,15 @@
 <?php
+use App\Models\Role;
 use App\Models\Permission;
 
 describe('Permissions', function(){
     it('has basic permissions set up', function () {
+        //Create permissions
+        Permission::create(['name' => 'manage users']);
+
+        //Assign permissions to roles
+        Role::findByName('admin')->givePermissionTo('manage users');
+
         expect(count(Permission::all()))->toBeGreaterThan(0);
     });
 });
